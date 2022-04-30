aeefa45e3c9895545fcf3b811c083e60d9362c23
nnnkkk7 <kurodanaoki0711pana@gmail.com>
2022-04-30 17:39:39 +0900

    add marshal json

---
+++ product-api/data/product.go
@@ -0,0 +1,39 @@
+package data
+
+import "time"
+
+type Product struct {
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float32 `json:"price"`
+	SKU         string  `json:"sku"`
+	CreatedOn   string  `json:"-"`
+	UpdatedOn   string  `json:"-"`
+	DeletedOn   string  `json:"-"`
+}
+
+func GetProducts() []*Product {
+	return productList
+}
+
+var productList = []*Product{
+	&Product{
+		ID:          1,
+		Name:        "latte",
+		Description: "milky coffee",
+		Price:       2.45,
+		SKU:         "abc323",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	&Product{
+		ID:          2,
+		Name:        "espresso",
+		Description: "strong coffee",
+		Price:       1.99,
+		SKU:         "ds345",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
